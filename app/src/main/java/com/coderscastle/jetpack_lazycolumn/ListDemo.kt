4b30e60513d191f00c6e71d1b9dbbe60d510c8d9
limon <limonreza3.1416@gmail.com>
2024-10-03 18:50:09 +0600

    lazy column with item and items

--- app/src/main/java/com/coderscastle/jetpack_lazycolumn/ListDemo.kt
+++ app/src/main/java/com/coderscastle/jetpack_lazycolumn/ListDemo.kt
@@ -1,6 +1,5 @@
 package com.coderscastle.jetpack_lazycolumn
 
-import android.graphics.Paint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -10,16 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
