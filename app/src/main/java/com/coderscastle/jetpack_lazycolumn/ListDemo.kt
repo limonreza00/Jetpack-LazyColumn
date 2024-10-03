f7a73c5c2667103658abcb69e89464cec9c9eeb6
limon <limonreza3.1416@gmail.com>
2024-10-03 17:16:13 +0600

    normal scroll using column vertical scroll modifier

--- app/src/main/java/com/coderscastle/jetpack_lazycolumn/ListDemo.kt
+++ app/src/main/java/com/coderscastle/jetpack_lazycolumn/ListDemo.kt
@@ -1,9 +1,27 @@
 package com.coderscastle.jetpack_lazycolumn
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 @Composable
 fun ListDemo (){
 
+    var scrollState = rememberScrollState()
+
+    Column (
+        modifier = Modifier.verticalScroll(scrollState)
+    )
+    {
+
+        for (i in 1..100){
+
+            Text(text = "Item $i")
+
+        }
+    }
 }
